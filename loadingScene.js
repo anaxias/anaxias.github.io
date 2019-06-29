0d35aa5322a707389115c8e51d66d3390f6f8fe4
Anna Sikkink <asikkink@hawaii.edu>
2019-06-29 00:28:19 -1000

    pls work T^T

--- loadingScene.js
+++ loadingScene.js
@@ -67,10 +67,10 @@ class loadingScene extends Phaser.Scene{
 
 		
 		backgroundMusic = this.sound.add('bgm');
-		success_sfx = this.sound.add('success');
-		error_sfx = this.sound.add('error');
 		backgroundMusic.loop = true;
 		backgroundMusic.play();
+		success_sfx = this.sound.add('success');
+		error_sfx = this.sound.add('error');
 		
 		timer = this.time.addEvent({
 			delay: Phaser.Math.Between(1000,8000),                // ms
@@ -217,15 +217,15 @@ class loadingScene extends Phaser.Scene{
 		
 		if(message){
 			
-			//console.log(message);
-			//console.log(channel);
+			console.log(message);
+			console.log(channel);
 			
 			if((message.texture.key == "test-msg3" && channel.texture.key == "general")
 			|| (message.texture.key == "test-msg4" && channel.texture.key == "art") 
 			|| (message.texture.key == "test-msg2" && channel.texture.key == "music") 
 			|| (message.texture.key == "test-msg5" && channel.texture.key == "prog") 
 			|| (message.texture.key == "test-msg" && channel.texture.key == "memes")){
-				//console.log("overlap is good! channel: "+channel+", message: "+message.name+", "+message.texture.key);
+				console.log("overlap is good! channel: "+channel+", message: "+message.name+", "+message.texture.key);
 				this.sound.play('success');
 				totalHP +=2;
 				message_array.splice(parseInt(message.name), 1);
